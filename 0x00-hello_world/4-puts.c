859f4c7e092e0541e82a2929bbc6ba6aea24aaaa
EMMANUEL <chigoziee087@gmail.com>
2023-03-20 15:00:01 -0700

    Tis is my first c written code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This function serves as the entry point of a C program
+ *
+ * Return: always
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
+}

13004ee65baad5efe536245bdc139083b9b7118c
EMMANUEL <chigoziee087@gmail.com>
2023-04-21 16:59:41 -0700

    Upper and lower cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main - This code prints alphabets in upper as well as lowercases
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+
+{
+	char Uplow;
+
+	for (Uplow = 'a'; Uplow <= 'z';)
+	putchar(Uplow);
+
+	for (Uplow = 'A'; Uplow <= 'Z';)
+	putchar(Uplow);
+	putchar('\n');
+
+	return (0);
+}

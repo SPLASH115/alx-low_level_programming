558f027dbbe2f99ea3c569e982fa2c7126e1c06b
Emmanuel <chigoziee087@gmail.com>
2023-09-23 15:53:24 -0700

    Lower cases are being printed

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of this code
+ *
+ * Return: Always success (0)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
